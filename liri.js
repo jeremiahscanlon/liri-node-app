24e0a01d354056220c3d6aa47f86f33165e82637
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2016-04-05 00:20:58 -0400

    creation and updating of log.txt

--- liri.js
+++ liri.js
@@ -31,11 +31,22 @@ var action = arguments[0];
 var data = arguments[1];
 
 // =====================================================================
+// Function to append data to log.txt file
+// =====================================================================
+
+function append(append) {
+    fs.appendFile('log.txt', append, (err) => {
+        if (err) throw err;
+    });
+}
+
+// =====================================================================
 // Main function
 // =====================================================================
 
 // create a function that takes the input of an action and data.
 function doit(action,data) {
+    append('\n***** New Search - '+action+' - '+data+' *****\n\n');
     // create a switch case function based on the action from the input
     switch(action) {
         // if the action is "my-tweets"
@@ -52,16 +63,20 @@ function doit(action,data) {
                         for (var i = 0; i < tweets.length; i++) {
                             console.log(tweets[i].text);
                             console.log('----------------------------------------------------');
+                            append(tweets[i].text+'\n');
+                            append('----------------------------------------------------\n');
                         } 
                     // if there are zero tweets
                     } else {
                         // tell the user to choose a new handle
                         console.log('Please search for another Twitter handle.')
+                        append('Please search for another Twitter handle.\n');
                     }
                 // if there is an error
                 } else {
                     // tell the user the error
                     console.log('Error: '+error);
+                    append('Error: '+error+'\n');
                 }
             });
             // end the switch case here
@@ -74,6 +89,7 @@ function doit(action,data) {
                 // if there is an error console log it
                 if ( err ) {
                     console.log('Error occurred: ' + err);
+                    append('Error: '+err+'\n');
                     return;
                 }
                 // if there are results
@@ -83,11 +99,16 @@ function doit(action,data) {
                     console.log('Song Name: '+results.tracks.items[0].name);
                     console.log('Spotify URL: '+results.tracks.items[0].artists[0].external_urls.spotify);
                     console.log('Album Name: '+results.tracks.items[0].album.name);
+                    append('Artist Name: '+results.tracks.items[0].artists[0].name+'\n');
+                    append('Song Name: '+results.tracks.items[0].name+'\n');
+                    append('Spotify URL: '+results.tracks.items[0].artists[0].external_urls.spotify+'\n');
+                    append('Album Name: '+results.tracks.items[0].album.name+'\n');
                 
                 // if there are no results
                 } else {
                     // tell the user to choose a new song
                     console.log('We did not find any results for that song.');
+                    append('We did not find any results for that song.\n');
                 } 
             });
             // end the switch case here
@@ -104,26 +125,37 @@ function doit(action,data) {
                     // console log the specific results that we want
                     // Title
                     console.log('Title: '+body.Title);
+                    append('Title: '+body.Title+'\n');
                     // Year
                     console.log('Year: '+body.Year);
+                    append('Year: '+body.Year+'\n');
                     // IMDB Rating
                     console.log('IMDB Rating: '+body.imdbRating);
+                    append('IMDB Rating: '+body.imdbRating+'\n');
                     // Country
                     console.log('Country: '+body.Country);
+                    append('Country: '+body.Country+'\n');
                     // Language
                     console.log('Language: '+body.Language);
+                    append('Language: '+body.Language+'\n');
                     // Plot
                     console.log('Short Plot: '+body.Plot);
+                    append('Short Plot: '+body.Plot+'\n');
                     // Actors
                     console.log('Actors: '+body.Actors);
+                    append('Actors: '+body.Actors+'\n');
                     // Rotten Tomatoes Rating
                     console.log('Rotten Tomatoes Rating: '+body.tomatoUserRating);
+                    append('Rotten Tomatoes Rating: '+body.tomatoUserRating+'\n');
                     // Rotton Tomatoes UrL
                     console.log('Rotton Tomatoes URL: '+body.tomatoURL);
+                    append('Rotton Tomatoes URL: '+body.tomatoURL+'\n');
                 // if there is an error
                 } else {
                     // tell the user the error
                     console.log('Error: '+error);
+                    append('Error: '+error+'\n');
+
                 }
             })
             // end the switch case here
@@ -147,6 +179,7 @@ function doit(action,data) {
         default:
             // ask them to submit a valid action
             console.log('That is not a valid action.');
+            append('That is not a valid action.\n');
     }
 }
 
